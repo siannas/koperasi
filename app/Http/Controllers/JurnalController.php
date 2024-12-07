656206e1b2bbbd0422ca809b7dbad2076ce26ce1
siannas <massive.annas@gmail.com>
2024-12-07 14:20:54 +0700

    fix: minor not null check date

--- app/Http/Controllers/JurnalController.php
+++ app/Http/Controllers/JurnalController.php
@@ -25,9 +25,9 @@ class JurnalController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->input('dateawal')) {
+        if ($request->input('dateawal') != null) {
             $this->filterDate = Carbon::createFromLocaleIsoFormat('!MMMM/Y', 'id', $request->input('dateawal') . "/" . $this->year);
-        } elseif ($request->cookie('date_month')) {
+        } elseif ($request->cookie('date_month') != null) {
             $this->filterDate = Carbon::createFromLocaleIsoFormat('!MMMM/Y', 'id', $request->cookie('date_month') . "/" . $this->year);
         } else {
             $this->filterDate = Carbon::createFromLocaleIsoFormat('!M/Y', 'id', date('n') . "/" . $this->year);
@@ -67,9 +67,9 @@ class JurnalController extends Controller
 
     public function data(Request $request)
     {
-        if ($request->input('dateawal')) {
+        if ($request->input('dateawal') != null) {
             $this->filterDate = Carbon::createFromLocaleIsoFormat('!MMMM/Y', 'id', $request->input('dateawal') . "/" . $this->year);
-        } elseif ($request->cookie('date_month')) {
+        } elseif ($request->cookie('date_month') != null) {
             $this->filterDate = Carbon::createFromLocaleIsoFormat('!MMMM/Y', 'id', $request->cookie('date_month') . "/" . $this->year);
         } else {
             $this->filterDate = Carbon::createFromLocaleIsoFormat('!M/Y', 'id', date('n') . "/" . $this->year);
